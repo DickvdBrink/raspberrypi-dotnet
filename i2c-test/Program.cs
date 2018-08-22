7f78467df7f98596b19b29767cba3da6811d083c
Dick van den Brink <d_vandenbrink@outlook.com>
2018-08-22 20:31:49 +0200

    Add empty .NET core project

---
+++ i2c-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace i2c_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
